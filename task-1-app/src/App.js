50aba9a4a9cf7c591d11d401712ccc2bbf6c176a
Aliaksandr Laskauneu <aliaksandr_laskauneu@epam.com>
2020-07-24 18:31:10 +0300

    All types of components

---
+++ task-1-app/src/App.js
@@ -0,0 +1,38 @@
+import React, {Component, PureComponent} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class StrictComponent extends Component{
+  render(){
+    return <h1>Hello from Strict Comp</h1>
+  }
+}
+
+class MyPureComponent extends PureComponent{
+  render(){
+    return <h1>Hello from Pure Component</h1>
+  }
+}
+
+let LambdaFuncComp = ()=> <h1>Hello from lambda function Component</h1>
+
+let FunctionalComp = function(){
+  return <h1>Hello from Functional Component</h1>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <StrictComponent/>
+        <MyPureComponent/>
+        <LambdaFuncComp/>
+        <FunctionalComp/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
